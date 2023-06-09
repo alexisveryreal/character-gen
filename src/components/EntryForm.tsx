b354fc3be70d36bcb6a8e653892d33577e850fb3
alexisveryreal <alexmaple20@gmail.com>
2023-06-09 13:44:47 -0700

    added react hook form + starting components have the form setup for the summon text will need to work on the emoji creation next

---
+++ src/components/EntryForm.tsx
@@ -0,0 +1,73 @@
+import Link from "next/link";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useZodForm,
+} from "./ui/Form";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { useToast } from "./ui/use-toast";
+
+const formSchema = z.object({
+  summonText: z.string().min(2, {
+    message: "Summon text must be at least 2 characters.",
+  }),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+export const EntryForm = () => {
+  const { toast } = useToast();
+
+  const form = useZodForm({
+    schema: formSchema,
+    defaultValues: {
+      summonText: "",
+    },
+  });
+
+  const onSubmit = (values: FormSchema) => {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      ),
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit, console.log)}
+        className="space-y-8"
+      >
+        <FormField
+          control={form.control}
+          name="summonText"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Summon Text</FormLabel>
+              <FormControl>
+                <Input placeholder="summon text here" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the text that will be inside the summon circle.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,28 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { Inter } from "next/font/google";
+import { Toaster } from "~/components/ui/toaster";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-sans: ${inter.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 };
 
 export default MyApp;
